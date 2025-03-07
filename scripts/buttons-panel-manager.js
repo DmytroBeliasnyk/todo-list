0ecc5414fc8c28d1408a92d40a7b076aecbb8baf
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2025-03-07 03:49:44 +0100

    implemented done and remove buttons

--- scripts/buttons-panel-manager.js
+++ scripts/buttons-panel-manager.js
@@ -1,12 +1,18 @@
 export const buttonsPanelManager = {
   formWrapper: document.querySelector(".form-wrapper"),
+  tasks: document.querySelector(".content__tasks"),
   "content__open-form-btn": function () {
     this.formWrapper.hidden = false
   },
   "content__done-btn": function () {
+    const selected = this.tasks.querySelectorAll(".selected")
+    for (const selectedElement of selected) {
+      selectedElement.classList.remove("selected")
+      selectedElement.classList.add("done")
+    }
   },
   "content__remove-btn": function () {
-    const selected = this.formWrapper.querySelectorAll(".selected")
+    const selected = this.tasks.querySelectorAll(".selected")
     for (const selectedElement of selected) {
       selectedElement.remove()
     }
