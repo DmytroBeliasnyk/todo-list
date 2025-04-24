4dcd5f711d49fd282ee148b46c7b22ef5454ada4
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2025-04-24 13:19:08 +0200

    refactoring render service

--- js/services/render.js
+++ js/services/render.js
@@ -1,54 +1,92 @@
 export default (taskContainer, tasksLoader) => {
+  const _addShowMenuListener = (showMenuButton, menu) => {
+    showMenuButton.addEventListener("click", event => {
+      const isOpen = showMenuButton.classList.toggle("show")
+
+      let menuScrollHeight = menu.scrollHeight
+      if (!isOpen) {
+        menuScrollHeight = 0
+      }
+
+      menu.style.height = menuScrollHeight + "px"
+    })
+  }
+  const _addCustomEventListener = (element, eventType, eventName, detail) => {
+    element.addEventListener(eventType, () => {
+      element.dispatchEvent(new CustomEvent(eventName, {
+        bubbles: true,
+        detail: detail
+      }))
+    })
+  }
   const _createTaskElement = (task) => {
+    const taskContentWrapper = document.createElement("div")
+    taskContentWrapper.className = "task__content-wrapper"
+
+    const taskLeftColumn = document.createElement("div")
+    taskLeftColumn.className = "task__left-column"
+
+    const taskName = document.createElement("div")
+    taskName.className = "task__name"
+    taskName.textContent = task.name
+
+    const taskShowMenuButton = document.createElement("div")
+    taskShowMenuButton.className = "task__show-menu-btn"
+
+    taskLeftColumn.append(taskName, taskShowMenuButton)
+
+    const taskRightColumn = document.createElement("div")
+    taskRightColumn.className = "task__right-column"
+
+    const taskStatus = document.createElement("div")
+    taskStatus.className = "task__status"
+    taskStatus.textContent = task.status
+
+    taskRightColumn.appendChild(taskStatus)
+
+    taskContentWrapper.append(taskLeftColumn, taskRightColumn)
+
+    const taskMenuWrapper = document.createElement("div")
+    taskMenuWrapper.className = "task__menu-wrapper"
+
+    const taskDescription = document.createElement("textarea")
+    taskDescription.className = "task__description scrolling"
+    taskDescription.textContent = task.description
+
+    taskMenuWrapper.appendChild(taskDescription)
+
+    const taskButtons = document.createElement("div")
+    taskButtons.className = "task__buttons"
+
+    const taskDoneButton = document.createElement("div")
+    taskDoneButton.className = "task__done button click-animation"
+    taskDoneButton.textContent = "Done"
+
+    const taskRemoveButton = document.createElement("div")
+    taskRemoveButton.className = "task__remove button click-animation"
+    taskRemoveButton.textContent = "Delete"
+
+    taskButtons.append(taskDoneButton, taskRemoveButton)
+
+    taskMenuWrapper.appendChild(taskButtons)
+
     const taskElement = document.createElement("div")
     taskElement.className = "task"
     if (task.status === "Done") {
       taskElement.classList.add("done")
+      taskDoneButton.disabled = true
     }
 
-    const leftContent = document.createElement("div")
-    leftContent.className = "task__left-content"
+    taskElement.append(taskContentWrapper, taskMenuWrapper)
 
-    const name = document.createElement("div")
-    name.className = "task__name"
-    name.textContent = task.name
-
-    const description = document.createElement("div")
-    description.className = "task__description"
-    description.textContent = task.description
-
-    leftContent.append(name, description)
-
-    const rightContent = document.createElement("div")
-    rightContent.className = "task__right-content"
-
-    const status = document.createElement("div")
-    status.className = "task__status"
-    status.textContent = task.status ?? "In progress"
-
-    rightContent.appendChild(status)
-
-    const wrapper = document.createElement("div")
-    wrapper.className = "task__content-wrapper"
-    wrapper.append(leftContent, rightContent)
-
-    const editButton = document.createElement("div")
-    editButton.className = "task__edit button"
-
-    const editIcon = document.createElement("div")
-    editIcon.className = "fa-solid fa-pen-to-square"
-
-    editButton.appendChild(editIcon)
-
-    const removeButton = document.createElement("div")
-    removeButton.className = "task__remove"
-
-    const removeIcon = document.createElement("div")
-    removeIcon.className = "fa-solid fa-trash"
-
-    removeButton.appendChild(removeIcon)
-
-    taskElement.append(wrapper, editButton, removeButton)
+    const detail = {
+      task: task,
+      taskElement: taskElement
+    }
+    _addShowMenuListener(taskShowMenuButton, taskMenuWrapper)
+    _addCustomEventListener(taskDescription, "change", "update-description", detail)
+    _addCustomEventListener(taskDoneButton, "click", "done-task", detail)
+    _addCustomEventListener(taskRemoveButton, "click", "remove-task", detail)
 
     return taskElement
   }
@@ -90,26 +128,13 @@ export default (taskContainer, tasksLoader) => {
         this.renderPage()
       }
     },
-    updateTask(task) {
-      const tasks = Array.from(taskContainer.querySelectorAll(".task"))
-
-      const findTask = tasks.find(findElement =>
-        findElement.querySelector(".task__name").textContent === task.name)
-
-      findTask.querySelector(".task__description").textContent = task.description
-      if (task.status === "Done") {
-        const taskStatus = findTask.querySelector(".task__status")
-
-        taskStatus.textContent = task.status
-        taskStatus.classList.add("done")
-      }
+    updateTaskStatus(taskElement) {
+      taskElement.classList.add("done")
+      taskElement.querySelector(".task__status").textContent = "Done"
+      taskElement.querySelector(".task__done").disabled = true
     },
-    removeTaskByName(taskName) {
-      const tasks = Array.from(taskContainer.querySelectorAll(".task"))
-
-      tasks.find(findElement =>
-        findElement.querySelector(".task__name").textContent === taskName)
-        .remove()
+    removeTask(taskElement) {
+      taskElement.remove()
     },
   }
 }
