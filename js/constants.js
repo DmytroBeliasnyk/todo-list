17bbe4799ff6014dd419f24fb94567d4425f668d
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2025-05-03 16:47:10 +0200

    add constants.js

---
+++ js/constants.js
@@ -0,0 +1,24 @@
+export const constants = {
+  form: {
+    messages: {
+      formEmptyName: "Field \"name\" can't be empty.",
+    },
+    actions: {
+      addTask: "add",
+      editTask: "edit",
+    },
+  },
+  filters: {
+    ids: {
+      searchInput: "search",
+      inProgress: "filter-in-progress",
+      done: "filter-done",
+    },
+  },
+  tasks: {
+    status: {
+      inProgress: "In progress",
+      done: "Done",
+    }
+  },
+}
