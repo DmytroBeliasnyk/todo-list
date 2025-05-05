2dbbd4ad0be53792793202e0e202e18f594b9e1b
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2025-05-05 12:29:54 +0200

    refactoring task form

--- js/components/tasks/form.js
+++ js/components/tasks/form.js
@@ -1,5 +1,5 @@
 import FormService from "../../services/form.js";
-import {constants} from "../../utils/constants.js";
+import {form} from "../../utils/constants.js";
 
 const modal = document.querySelector("#task-form-modal")
 const modalContainer = modal.parentNode
@@ -11,8 +11,8 @@ export function taskFormInit(formAddCallback) {
   document.querySelector(".open-task-form-add-task")
     .addEventListener("click", () => {
       openTaskForm({
-        action: constants.form.actions.addTask,
-        formAddCallback: (task) => {
+        action: form.actions.addTask,
+        formSubmitCallback: (task) => {
           task.id = crypto.randomUUID()
 
           formAddCallback(task)
@@ -26,27 +26,21 @@ export function openTaskForm(options) {
   modal.classList.add(options.action)
   form.elements.name.focus()
 
-  let formOnSubmit = null
-  if (options.formAddCallback && typeof options.formAddCallback === "function") {
-    formOnSubmit = options.formAddCallback
-  }
-
-  if (options.formEditCallback && typeof options.formEditCallback === "function") {
-    formOnSubmit = options.formEditCallback
+  if (options.task) {
     form.elements.name.value = options.task.name
-    form.elements.description.value = options.task.description
+    form.elements.description.value = options.task.description ?? ''
   }
 
   formService.init(
     task => {
       const taskName = task.name.trim()
       if (!taskName) {
-        formService.setError(constants.form.messages.formEmptyName)
+        formService.setError(form.messages.formEmptyName)
         return
       }
 
       task.name = taskName
-      formOnSubmit(task)
+      options.formSubmitCallback(task)
 
       form.reset()
     },
