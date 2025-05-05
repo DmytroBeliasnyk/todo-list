a96ce1115dab19a4df8bdd727f9ec34ef9cf1f12
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2025-05-05 12:10:44 +0200

    refactor pagination.js

--- js/utils/pagination.js
+++ js/utils/pagination.js
@@ -1,21 +1,20 @@
-export function createPages(elementsOnPage) {
-  const _elementsOnPage = elementsOnPage
-  let _currentPage = 0
-  let _pageCount = 0
+export function createPages(countOnPage) {
+  let _current = 0
+  let _count = 0
 
   return {
-    * pageGenerator() {
-      while (_currentPage <= _pageCount) {
-        const start = _currentPage * _elementsOnPage
-        const end = start + _elementsOnPage
+    * nextPage() {
+      while (_current <= _count) {
+        const start = _current * countOnPage
+        const end = start + countOnPage
 
-        _currentPage++
+        _current++
         yield {start, end}
       }
     },
-    setNewPages(elementsCount) {
-      _currentPage = 0
-      _pageCount = Math.ceil(elementsCount / _elementsOnPage)
+    newPages(elementsCount) {
+      _current = 0
+      _count = Math.ceil(elementsCount / countOnPage)
     },
   }
 }
