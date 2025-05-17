a7c80b325eb5ba73ac1bb0b0cf295fda106bc9b9
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2025-05-17 15:08:43 +0200

    implement debounce tests

---
+++ js/utils/__tests__/debounce.test.js
@@ -0,0 +1,22 @@
+import {jest} from "@jest/globals"
+import {debounce} from "../debounce.js";
+
+const fn = jest.fn(done => {
+  expect(fn).toHaveBeenCalled()
+  done()
+})
+
+let debounceFn
+beforeEach(() => {
+  debounceFn = debounce(fn, 500)
+})
+
+test.only("debounce: one call", done => {
+  debounceFn(done)
+})
+
+test("debounce: several calls", done => {
+  for (let i = 0; i < 10; i++) {
+    debounceFn(done)
+  }
+})
